4846d8ed8902b8ea69e9d43fc539759eaab0ac4d
Arnold Vink <info@arnoldvink.com>
2023-02-22 16:41:13 +0100

    - Adding .NET version to debug AVCode.

---
+++ NET/ProcessTool/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessTool
+{
+    public partial class ProcessTool
+    {
+        private static int Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to the process tool.");
+
+                //Check launch arguments
+                if (args.Count() < 2)
+                {
+                    Console.WriteLine("Command line arguments:");
+
+                    Console_WaitForInput();
+                    return 0;
+                }
+
+                //Filter launch arguments
+
+                //Wait for user key input
+                Console_WaitForInput();
+            }
+            catch { }
+            return 0;
+        }
+
+        private static void Console_WaitForInput()
+        {
+            try
+            {
+                Console.WriteLine("\nPress any key to close the process tool . . .");
+                Console.ReadKey();
+            }
+            catch { }
+        }
+    }
+}
---
+++ NET/ProcessTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+//General information about assembly
+[assembly: AssemblyTitle("ProcessTool")]
+[assembly: AssemblyProduct("ProcessTool")]
+[assembly: AssemblyDescription("ProcessTool")]
+[assembly: AssemblyCompany("Arnold Vink")]
+[assembly: AssemblyCopyright("Copyright © Arnold Vink")]
+
+//Version information about assembly 
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
